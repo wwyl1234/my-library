1851cc6ccb70c517814100915fb282be9cfd4cfb
Winnie Lam <winnielam@Winnies-MacBook-Air.local>
2020-02-19 21:40:09 -0500

    Added github authentication sample code from javascript.

--- scripts/main.js
+++ scripts/main.js
@@ -13,5 +13,27 @@ const FIREBASECONFIG = {
 // Initialize Firebase
 firebase.initializeApp(FIREBASECONFIG);
 firebase.analytics();
-let database = firebase.database();
-console.log(database);
+
+
+
+var provider = new firebase.auth.GithubAuthProvider();
+
+firebase.auth().signInWithPopup(provider).then(function(result) {
+    // This gives you a GitHub Access Token. You can use it to access the GitHub API.
+    var token = result.credential.accessToken;
+    // The signed-in user info.
+    var user = result.user;
+    // ...
+  }).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+  });
+  
+  let database = firebase.database();
+  console.log(database);

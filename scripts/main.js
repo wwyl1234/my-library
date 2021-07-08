1371b9f1b705ba57d198c77914112fb5f8bdcbb9
Winnie Lam <winnielam@Winnies-MacBook-Air.local>
2021-07-08 19:14:41 -0400

    Add Get Data button and functionality to download database as a backup if user is authorized

--- scripts/main.js
+++ scripts/main.js
@@ -19,9 +19,9 @@ $(document).ready(function() {
 
     // Load datatabase into table.
     // Data from https://my-library-bd9da.firebaseio.com/books/.json is an array of objects.
-    var table = $('#booksTable').DataTable( {
+    var table = $("#booksTable").DataTable( {
         "ajax": {
-            "url": 'https://my-library-bd9da.firebaseio.com/books/.json',
+            "url": "https://my-library-bd9da.firebaseio.com/books/.json",
             "method": "GET",
             "dataSrc": ""
             },
@@ -37,42 +37,43 @@ $(document).ready(function() {
     } );
 
     // This can be use to filter the table and  filter by borrowed is false 
-    table.column(3).search('false', true, false).draw();
+    table.column(3).search("false", true, false).draw();
     // This can be use to filter the table and  filter by needAuth is false 
-    table.column(5).search('false', true, false).draw();
+    table.column(5).search("false", true, false).draw();
     
     // Add book to database, when user click submit button on the form.
     $(function(){
-        $('#addBookForm').on('submit', function(e){
+        $("#addBookForm").on("submit", function(e){
           e.preventDefault();
           let postData = getAddBookData();
           console.log(postData);
           addBookToDatabase(postData);
           
           // need to reload table with a timeout of 2 minutes
-          let reload = $('#booksTable').DataTable().ajax.reload
+          let reload = $("#booksTable").DataTable().ajax.reload
           setTimeout(reload, 7200);
         
           // need to close window
-          $('#addBookModal').modal('toggle');
+          $("#addBookModal").modal("toggle");
         });
     });
 
     // Login, when user click submit button on the form.
     $(function(){
-        $('#loginForm').on('submit', function(e){
+        $("#loginForm").on("submit", function(e){
             e.preventDefault();
-            let email = $('#email').val();
-            let password = $('#password').val();
+            let email = $("#email").val();
+            let password = $("#password").val();
             firebase.auth().signInWithEmailAndPassword(email, password)
                 .then((userCredential) => {
                     // Signed in
-                    $('#addBookButton').css("visibility", "visible");
-                    $('#updateBookButton').css("visibility", "visible");
+                    $("#addBookButton").css("visibility", "visible");
+                    $("#updateBookButton").css("visibility", "visible");
+                    $("#getBackupButton").css("visibility", "visible");
                     
                     // Undo the filters for unauthorized users
-                    table.search('')
-                        .columns().search('')
+                    table.search("")
+                        .columns().search("")
                         .draw();
                 })
                 .catch((error) => {
@@ -82,14 +83,14 @@ $(document).ready(function() {
                 });
           
           // need to close window
-          $('#loginModal').modal('toggle');
+          $("#loginModal").modal("toggle");
         });
     });
 
     // Get book data , when user click on Get Data button in the update book modal dialog
-    $('#getBookDataButton').click(function(){
-        console.log('pressed');
-        let isbn = $('#updateIsbn').val();
+    $("#getBookDataButton").click(function(){
+        console.log("pressed");
+        let isbn = $("#updateIsbn").val();
         if (isValidIsbn13(isbn)){
             getBookObj(isbn)
                 .then(result => {
@@ -98,7 +99,7 @@ $(document).ready(function() {
                     element.html(JSON.stringify(result[Object.keys(result)[0]], undefined, 2));
                 })
                 .catch(error => {
-                    console.warn('ISBN not in database!');
+                    console.warn("ISBN not in database!");
                 });
         } else {
             console.warn("Invalid isbn13")
@@ -107,19 +108,19 @@ $(document).ready(function() {
     
     // Update book, when user click submit button on the form.
     $(function(){
-        $('#updateBookForm').on('submit', function(e){
+        $("#updateBookForm").on("submit", function(e){
             e.preventDefault();
-            let isbn = $('#updateIsbn').val();
-            let property = $('#property').val();
-            let value = $('#updateValue').val();
+            let isbn = $("#updateIsbn").val();
+            let property = $("#property").val();
+            let value = $("#updateValue").val();
             updateBook(isbn, property, value)
 
             // need to reload table with a timeout of 2 minutes
-            let reload = $('#booksTable').DataTable().ajax.reload
+            let reload = $("#booksTable").DataTable().ajax.reload
             setTimeout(reload, 7200);
           
           // need to close window
-            $('#updateBookModal').modal('toggle');
+            $("#updateBookModal").modal("toggle");
         });
     });
 
@@ -127,30 +128,30 @@ $(document).ready(function() {
 
 // Returns a JSON of a book to be added to the database
 function getAddBookData(){
-    let name = $('#name').val();
-    let author = $('#author').val();
-    let location = $('#location').val();
+    let name = $("#name").val();
+    let author = $("#author").val();
+    let location = $("#location").val();
     let borrowed = null;
-    let isbn = $('#isbn').val();
+    let isbn = $("#isbn").val();
     let needAuth = null; 
 
     // Deal with the radio check box for borrowed property
-    if ($('#borrowedSetTrue').is(':checked'))
+    if ($("#borrowedSetTrue").is(":checked"))
     {
         borrowed = true;
     }
 
-    if ($('#borrowedSetFalse').is(':checked')){
+    if ($("#borrowedSetFalse").is(":checked")){
         borrowed = false;
     }
 
     // Deal with the radio check box for needAuth property
-    if ($('#needAuthSetTrue').is(':checked'))
+    if ($("#needAuthSetTrue").is(":checked"))
     {
         needAuth = true;
     }
 
-    if ($('#needAuthSetFalse').is(':checked')){
+    if ($("#needAuthSetFalse").is(":checked")){
         needAuth = false;
     }
 
@@ -160,18 +161,18 @@ function getAddBookData(){
     }
 
     // Deal with the radio check box for ISBN13 or ISBN10
-    if ($('#isbn10').is(':checked'))
+    if ($("#isbn10").is(":checked"))
     {
         if(!isValidIsbn10(isbn)){
-            console.warn('User gave an invalid ISBN10.');
+            console.warn("User gave an invalid ISBN10.");
             return {};
         }
         isbn = convertIsbn10ToIsbn13(isbn);
     }
 
-    if ($('#isbn13').is(':checked')){
+    if ($("#isbn13").is(":checked")){
         if(!isValidIsbn13(isbn)){
-            console.warn('User gave an invalid ISBN13.');
+            console.warn("User gave an invalid ISBN13.");
             return {};
         }
     }
@@ -219,13 +220,13 @@ function addBookToDatabase(data){
 // Given the isbn in str format, return the promise containing the book object.
 async function getBookObj(isbn){
     if(!isValidIsbn13(isbn)){
-        throw 'User gave an invalid ISBN13.';
+        throw "User gave an invalid ISBN13.";
     }
     let db = firebase.database();
     let ref = db.ref("books");
 
     
-    let result = await ref.orderByChild('isbn').equalTo(isbn).once("value")
+    let result = await ref.orderByChild("isbn").equalTo(isbn).once("value")
         .then (snap  => {
             let obj = snap.val()
             console.log(obj)
@@ -234,7 +235,7 @@ async function getBookObj(isbn){
             return snap.val();
         })
         .catch(error => {
-            console.warn('ISBN not in database!')
+            console.warn("ISBN not in database!")
         })
     return result;
     
@@ -256,6 +257,23 @@ function updateBook(isbn, property, value){
             bookRef.set(data);
         })
         .catch(error => {
-            console.warn('ISBN is not in database!');
+            console.warn("ISBN is not in database!");
         })
 }
+
+
+// Download database 
+function downloadDatabase() {
+    let backup = {}
+    firebase.database().ref("books").once("value", function(snap){
+        backup["books"] = JSON.parse(JSON.stringify(snap))
+        // Creates a temporary HTML element
+        var element = document.createElement("a");
+        element.setAttribute("href", "data:text/plain;charset=utf-8," + encodeURIComponent(JSON.stringify(backup)));
+        element.setAttribute("download",  "backup.json");
+        element.style.display = "none";
+        document.body.appendChild(element);
+        element.click();
+        document.body.removeChild(element);
+    })
+  }
